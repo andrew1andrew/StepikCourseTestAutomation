d413f280ebb1900f6bb6703c7cb5813cb4a4080b
andrew1andrew <andrey-zheleznyak124@yandex.ru>
2023-12-15 18:01:19 +0300

    Editing mod3_les_6_task2

--- Module3_Lesson6_Task2/conftest.py
+++ Module3_Lesson6_Task2/conftest.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption("--language", action="store", default="fr", help="Choose language")
+    parser.addoption("--language", action="store", default="es", help="Choose language")
 
 
 @pytest.fixture(scope="function")
@@ -14,6 +14,5 @@ def browser(request):
     options = Options()
     options.add_experimental_option("prefs", {"intl.accept_languages": language})
     browser = webdriver.Chrome(options=options)
-    time.sleep(5)
     yield browser
     browser.quit()
--- Module3_Lesson6_Task2/test_items.py
+++ Module3_Lesson6_Task2/test_items.py
@@ -1,7 +1,7 @@
-import time
-
 from selenium.webdriver.common.by import By
+import time
 
 def test_should_present_button_add_to_basket(browser):
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+    time.sleep(30)
     assert (browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket").is_displayed()), "На странице нет кнопки добавления товара в корзину"
